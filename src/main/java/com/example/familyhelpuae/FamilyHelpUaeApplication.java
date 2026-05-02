c1969cdadc5e8f37af7913734565f32826872194
Sabrina X <sabrinaislam10@gmail.com>
2026-05-02 17:46:11 +0400

    first commit in Springboot from Sabrina

---
+++ src/main/java/com/example/familyhelpuae/FamilyHelpUaeApplication.java
@@ -0,0 +1,14 @@
+package com.example.familyhelpuae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyHelpUaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyHelpUaeApplication.class, args);
+		//Hi Hawrah!!!
+	}
+
+}
